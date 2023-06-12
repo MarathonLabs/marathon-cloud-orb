f4cad568bf92ae26ace60a90a67e4586221fc3a9
Anton Malinskiy <anton@malinskiy.com>
2023-06-13 00:09:46 +1000

    Fix secret variable indirection in sh (#4)
    * fix(test): sh variable indirection

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-API_KEY=$(eval "echo $ORB_API_KEY_ENV_VAR")
+API_KEY=$(eval echo \$"$ORB_API_KEY_ENV_VAR")
 
 marathon-cloud -api-key "$API_KEY" -apk "$ORB_APPLICATION" -testapk "$ORB_TEST_APPLICATION" -o "$ORB_OUTPUT"
