f6ed9518c5835de823f95214754f5c06cac4bc67
Anton Malinskiy <anton@malinskiy.com>
2023-06-12 23:54:21 +1000

    fix(test): add debug logging

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 API_KEY=$(eval "echo $ORB_API_KEY_ENV_VAR")
+echo $ORB_API_KEY_ENV_VAR
+echo $API_KEY
 
 marathon-cloud -api-key "$API_KEY" -apk "$ORB_APPLICATION" -testapk "$ORB_TEST_APPLICATION" -o "$ORB_OUTPUT"
