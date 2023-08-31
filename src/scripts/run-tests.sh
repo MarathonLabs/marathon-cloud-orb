a50c6eed007ecba65eb4c700a67d0635fd10ea30
Evgenii Matsiuk (Eugene Matsyuk) <eugene.matsyuk@gmail.com>
2023-08-31 16:24:28 +0300

    Os version support (#8)
    feat(os-version-support): osVersion parameter is supported plus other optional parameters were added

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -2,4 +2,19 @@
 
 API_KEY=$(eval echo \$"$ORB_API_KEY_ENV_VAR")
 
-marathon-cloud -api-key "$API_KEY" -app "$ORB_APPLICATION" -testapp "$ORB_TEST_APPLICATION" -o "$ORB_OUTPUT" -platform "$ORB_PLATFORM"
+command="marathon-cloud -api-key \"$API_KEY\" -app \"$ORB_APPLICATION\" -testapp \"$ORB_TEST_APPLICATION\" -o \"$ORB_OUTPUT\" -platform \"$ORB_PLATFORM\""
+
+if [ -n "$ORB_OS_VERSION" ]; then
+    command="$command -os-version \"$ORB_OS_VERSION\""
+fi
+
+if [ -n "$ORB_LINK" ]; then
+    command="$command -link \"$ORB_LINK\""
+fi
+
+if [ -n "$ORB_NAME" ]; then
+    command="$command -name \"$ORB_NAME\""
+fi
+
+echo "Executing command: $command"
+eval "$command"
